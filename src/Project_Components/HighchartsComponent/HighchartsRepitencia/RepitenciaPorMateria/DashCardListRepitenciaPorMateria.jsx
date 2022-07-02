481e64f4a34c98832d828c55970907ec4f80d6d9
unknown <calycus@hotmail.com>
2022-07-02 01:31:42 -0500

    Implementacion de nuevo checkedox en single selected en la seleccion de una unica materia para su analisis

--- src/Project_Components/HighchartsComponent/HighchartsRepitencia/RepitenciaPorMateria/DashCardListRepitenciaPorMateria.jsx
+++ src/Project_Components/HighchartsComponent/HighchartsRepitencia/RepitenciaPorMateria/DashCardListRepitenciaPorMateria.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Checkbox, Typography, InputBase, IconButton, TextField } from '@mui/material';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Checkbox, Typography, InputBase, IconButton, TextField, Radio } from '@mui/material';
 import { Search } from '@mui/icons-material';
 import Highcharts from "highcharts";
 import HcMore from "highcharts/highcharts-more";
-import MUIDataTable from "mui-datatables";
 
 //Dependencias
 import { selectArrayListMateriasRepitencia } from '../../../../store/HighchartStore/DashboardRepitencia/RepitenciaPorMateria/ListTableStoreRepitenciaPorMateria';
@@ -51,41 +50,133 @@ const TableOptions = {
 export default function SubjectDataTable() {
     viewRowsTable = useSelector(selectArrayListMateriasRepitencia)
 
-    const [chexData, sendChexData] = useState([])
-    const [contador, setContador] = useState(0)
 
     const [RowsTable, setRows] = useState([])
     const [Search, setSearch] = useState(false)
+    const [checked, setChecked] = useState(-1);
 
-    const handleSelects = (data, checkedControl) => {
-        console.log(checkedControl)
-        console.log(chexData)
-        sendChexData(data);
-
-        /* if (!checkedControl) {
-            newArray = chexData.filter((item) => item.id !== data.id);
-            sendChexData(newArray);
-            if (newArray.length >= 1) {
-                cont = newArray.length
-            }
-            return
-        }
-        chexData.push(data);
-        sendChexData(chexData);
-
-        if (chexData.length >= 1) {
-            cont = chexData.length
-        } */
+    const isDisabled = (data) => {
+        setChecked(data);
+    };
+    const handleSelects = (data) => {
+        console.log(data)
     }
 
-    const handleChexControl = (checked) => {
-        if (checked) {
-
-            setContador(contador + 1)
-        } else {
-
-            setContador(contador - 1)
+    const requestSearch = (searchedVal) => {
+        if (searchedVal.length == 0) {
+            setSearch(false)
+            return
         }
+        const filteredRows = viewRowsTable.filter((row) => {
+            return row.materia.toLowerCase().includes(searchedVal.toLowerCase());
+        });
+        setRows(filteredRows)
+        setSearch(true)
+    };
+
+    return (
+        <TableContainer component={Paper}>
+            <Box sx={{ display: 'flex', alignItems: "flex-end" }}>
+                <Typography
+                    className='TableTitleRepitenciaPorMateria'
+                    component="div"
+                >
+                    TABLA DE MATERIAS CON REPITENCIA
+                </Typography>
+                <TextField
+                    sx={{ display: 'flex', width: 400 }}
+                    id="standard-basic"
+                    label="Search"
+                    variant="standard"
+                    onChange={(e) => requestSearch(e.target.value)} />
+            </Box>
+            <Table sx={{ minWidth: 500 }} aria-label="simple table">
+                <TableHead style={{ display: "flex" }}>
+                    <TableRow
+                        style={{ width: '100%', display: 'grid', gridTemplateColumns: '3rem auto 5rem 7rem 9rem' }}>
+                        <TableCell style={{ content: " " }}></TableCell>
+                        <TableCell className='rowTableMateria'>Materia</TableCell>
+                        <TableCell className='rowTable' >Nivel</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody style={{ display: 'flex', flexDirection: 'column', overflow: 'auto', maxHeight: '350px' }}>
+                    {!Search ?
+                        viewRowsTable.map((row, index) => (
+                            <TableRow
+                                style={{ display: 'grid', gridTemplateColumns: '3rem auto 5rem 7rem 8rem', alignItems: 'center' }}
+                                key={index}
+                            >
+                                <TableCell className='rowTable' style={{ textAlign: 'center' }}>
+                                    <Checkbox
+                                        color="success"
+                                        checked={checked == index}
+                                        onChange={(e) => isDisabled(e.target.value)}
+                                        onClick={() => handleSelects(row)}
+                                        value={index}
+                                    />
+                                </TableCell>
+                                <TableCell className='rowTableMateria' >{row.materia}</TableCell>
+                                <TableCell className='rowTable' >{row.nivel}</TableCell>
+                            </TableRow>
+                        ))
+                        : RowsTable.map((row, index) => (
+                            <TableRow
+                                style={{ display: 'grid', gridTemplateColumns: '3rem auto 5rem 7rem 8rem', alignItems: 'center' }}
+                                key={index}
+                            >
+                                <TableCell className='rowTable' style={{ textAlign: 'center' }}>
+                                    <Checkbox
+                                        color="success"
+                                        checked={checked == index}
+                                        onChange={(e) => isDisabled(e.target.value)}
+                                        onClick={() => handleSelects(row)}
+                                        value={index}
+                                    />
+                                </TableCell>
+                                <TableCell className='rowTableMateria' >{row.materia}</TableCell>
+                                <TableCell className='rowTable' >{row.nivel}</TableCell>
+                            </TableRow>
+                        ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+/* const Fila = ({ row, handleSelects, handleChange, index, selectedValue }) => {
+    return (
+        <TableRow
+            style={{ display: 'grid', gridTemplateColumns: '3rem auto 5rem 7rem 8rem', alignItems: 'center' }}
+        >
+            <TableCell className='rowTable' style={{ textAlign: 'center' }}>
+                <Checkbox
+                    color="success"
+                    checked={selectedValue == index}
+                    onChange={(e) => handleChange(e.target.value)}
+                    onClick={() => handleSelects(row)}
+                    value={index}
+                />
+            </TableCell>
+            <TableCell className='rowTableMateria' >{row.materia}</TableCell>
+            <TableCell className='rowTable' >{row.nivel}</TableCell>
+        </TableRow>
+    )
+} */
+
+/* export default function SubjectDataTable() {
+    viewRowsTable = useSelector(selectArrayListMateriasRepitencia)
+
+
+    const [RowsTable, setRows] = useState([])
+    const [Search, setSearch] = useState(false)
+    const [selectedValue, setSelectedValue] = useState(-1);
+
+    const handleChange = (data) => {
+        setSelectedValue(data);
+        console.log(data);
+    };
+    const handleSelects = (data) => {
+        console.log(data)
     }
 
     const requestSearch = (searchedVal) => {
@@ -130,46 +221,28 @@ export default function SubjectDataTable() {
                         viewRowsTable.map((row, index) => (
                             <Fila
                                 key={index}
+                                index={index}
                                 row={row}
                                 handleSelects={handleSelects}
-                                handleChexControl={handleChexControl}
-                                contador={contador} />
+                                handleChange={handleChange}
+                            />
                         ))
                         : RowsTable.map((row, index) => (
                             <Fila
                                 key={index}
                                 row={row}
+                                selectedValue={selectedValue}
                                 handleSelects={handleSelects}
-                                handleChexControl={handleChexControl}
-                                contador={contador} />
+                                handleChange={handleChange}
+                            />
                         ))}
                 </TableBody>
             </Table>
         </TableContainer>
     )
-
-    /* return (
-        < MUIDataTable
-            title="TABLA DE MATERIAS CON REPITENCIA"
-            data={viewRowsTable}
-            columns={TableColumns}
-            options={TableOptions}
-            onChange={(e) => console.log(e.target.value)}
-        />
-    ) */
 }
 
-const Fila = ({ row, handleSelects, handleChexControl, contador }) => {
-    const [checked, setChecked] = useState(false)
-
-    const isDisabled = () => {
-        if (!checked && contador == 1) {
-            return true
-        } else {
-            return false
-        }
-    }
-
+const Fila = ({ row, handleSelects, handleChange, index, selectedValue }) => {
     return (
         <TableRow
             style={{ display: 'grid', gridTemplateColumns: '3rem auto 5rem 7rem 8rem', alignItems: 'center' }}
@@ -177,16 +250,14 @@ const Fila = ({ row, handleSelects, handleChexControl, contador }) => {
             <TableCell className='rowTable' style={{ textAlign: 'center' }}>
                 <Checkbox
                     color="success"
-                    checked={checked}
-                    disabled={isDisabled(checked)}
-                    onClick={() => handleSelects(row, !checked)}
-                    onChange={(e) => {
-                        handleChexControl(e.target.checked)
-                        setChecked(e.target.checked)
-                    }} />
+                    checked={selectedValue == index}
+                    onChange={(e) => handleChange(e.target.value)}
+                    onClick={() => handleSelects(row)}
+                    value={index}
+                />
             </TableCell>
             <TableCell className='rowTableMateria' >{row.materia}</TableCell>
             <TableCell className='rowTable' >{row.nivel}</TableCell>
         </TableRow>
     )
-}
+} */
